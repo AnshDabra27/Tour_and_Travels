9878b6bbffb2f03d4a57b47a796beb3d2ab31d80
AnshDabra27 <kambojansh2004@gmail.com>
2025-02-18 02:04:32 +0530

    hotel controllers

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
-import authRoute from "./routes/auth.js"
-import usersRoute from "./routes/users.js"
-import hotelsRoute from "./routes/hotels.js"
-import roomsRoute from "./routes/rooms.js"
+import authRoute from "./routes/authRoute.js"
+import usersRoute from "./routes/usersRoute.js"
+import hotelsRoute from "./routes/hotelsRoute.js"
+import roomsRoute from "./routes/roomsRoute.js"
 const app = express()
 dotenv.config()
 
@@ -35,6 +35,18 @@ app.use('/api/auth',authRoute);
 app.use('/api/users',usersRoute);
 app.use('/api/hotels',hotelsRoute);
 app.use('/api/rooms',roomsRoute);
+
+app.use((err,req,res,next)=>{
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong !"
+    return res.status(errorStatus).json({
+        success:false,
+        status:errorStatus,
+        message:errorMessage,
+        stack:err.stack,
+    })
+})
+
 
 app.get('/users',(req,res)=>{
     res.send("helloooooo first request")
